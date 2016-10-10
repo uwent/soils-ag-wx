90a9ee1c0d63591bf220ed9f31acac5415db0533
Allen Ballman <aballman@gmail.com>
2016-10-10 17:53:01 -0400

    Change whenever log location and set up job on deploy.

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,7 +20,7 @@
 # Learn more: http://github.com/javan/whenever
 
 
-set :output, "/tmp/whenever.log"
+set :output, "/tmp/mailer_whenever.log"
 set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
 job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
 
