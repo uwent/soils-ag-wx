0121bc0445735db21cf6f14e44012272c81e4e64
Allen Ballman <aballman@gmail.com>
2017-02-03 10:38:20 -0500

    Changed log level to warn in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,11 @@ SoilsAgWx::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
-  
+
   # While sendmail is less configurable than the default smtp, it works on our server
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = {host: 'agweather.cals.wisc.edu'}
-  
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -47,7 +47,7 @@ SoilsAgWx::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
