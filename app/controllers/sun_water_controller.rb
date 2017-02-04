999c863e986d9e4d346865de88d20aa23b8cf8cd
Allen Ballman <aballman@gmail.com>
2017-02-04 11:42:20 -0500

    Added download of day's worth of et values.

--- app/controllers/sun_water_controller.rb
+++ app/controllers/sun_water_controller.rb
@@ -4,7 +4,7 @@ class SunWaterController < ApplicationController
 
   before_filter :authenticate
   skip_before_filter :verify_authenticity_token, only: :get_grid
-  
+
   def index
   end
 
@@ -24,15 +24,10 @@ class SunWaterController < ApplicationController
   end
 
   def et_wimn
-    @grid_classes = grid_classes
-    begin
-      endpoint = "#{Endpoint::BASE_URL}/evapotranspirations/#{Time.now.yesterday.to_date.to_s}"
-      resp = HTTParty.get(endpoint, { timeout: 10 })
-      body = JSON.parse(resp.body)
-      @map_image = "#{Endpoint::HOST}#{body['map']}"
-    rescue Net::ReadTimeout
-      Rails.logger.error("Timeout on endpoint: #{endpoint}")
-      @map_image = ""
+    date = Date.yesterday
+    respond_to do |format|
+      format.html { et_wimn_image(date) }
+      format.csv { send_data et_wimn_csv(date), filename: "et-values-#{date}.csv" }
     end
   end
 
@@ -47,15 +42,44 @@ class SunWaterController < ApplicationController
 
   def spreadsheet_doc
   end
-  
+
   def grid_classes
     @grid_classes = GRID_CLASSES.select { |key,val| key == 'ET' }
   end
-  
+
   def grid_ets
     @title = 'Download ET Estimates'
     @grid_classes = grid_classes
   end
-  
-  
+
+  private
+    def et_wimn_image(date)
+      @grid_classes = grid_classes
+      begin
+        endpoint = "#{Endpoint::BASE_URL}/evapotranspirations/#{date}"
+        resp = HTTParty.get(endpoint, { timeout: 10 })
+        body = JSON.parse(resp.body)
+        @map_image = "#{Endpoint::HOST}#{body['map']}"
+      rescue Net::ReadTimeout
+        Rails.logger.error("Timeout on endpoint: #{endpoint}")
+        @map_image = ""
+      end
+    end
+
+    def et_wimn_csv(date)
+      begin
+        endpoint = "#{Endpoint::BASE_URL}/evapotranspirations/all_for_date?date=#{date.to_s}"
+        resp = HTTParty.get(endpoint, { timeout: 10 })
+        body = JSON.parse(resp.body)
+        return CSV.generate(headers: true) do |csv|
+          csv << %w(Latitude Longitude ET)
+          body.each do |hash|
+            csv << [hash['lat'], hash['long'].to_f * -1.0, hash['value']]
+          end
+        end
+      rescue Net::ReadTimeout
+        Rails.logger.error("Timeout on endpoint: #{endpoint}")
+        return CSV.generate(headers: true)
+      end
+    end
 end
