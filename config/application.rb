a9cf146476e35e6cf83dd8825297ffd75d29fb2a
bbryant7 <bailey.bryant10@gmail.com>
2020-11-09 13:25:33 -0600

    update load defaults

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,8 @@ Bundler.require(*Rails.groups)
 module SoilsAgWx
   class Application < Rails::Application
 
-    config.load_defaults 5.0
+    config.load_defaults 6.0
     config.time_zone = 'Central Time (US & Canada)'
-    
+
   end
 end
