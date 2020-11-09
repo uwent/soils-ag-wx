545ba46e5c33813eb4c44f2943ecac486faf63c6
bbryant7 <bailey.bryant10@gmail.com>
2020-11-09 16:18:09 -0600

    scenario typo

--- app/helpers/thermal_models_helper.rb
+++ app/helpers/thermal_models_helper.rb
@@ -11,17 +11,17 @@ module ThermalModelsHelper
     link_to link_text, "http://agwx.soils.wisc.edu/soils-agwx-assets/uwex_agwx/images/ddgifs/#{image_file}"
   end
 
-  def define_senario(dd_value, end_date)
+  def define_scenario(dd_value, end_date)
     date_string = end_date.year.to_s + "-07-15"
     july_15 = date_string.to_date
     case
-    when dd_value < 128.3 && end_date <= july_15 then return "senario_a"
-    when 215.8 > dd_value && dd_value >= 128.3 && end_date <= july_15 then "senario_b"
-    when 354.2 > dd_value && dd_value >= 215.8 && end_date <= july_15 then "senario_c"
-    when 507.2 > dd_value && dd_value >= 354.2 && end_date <= july_15 then "senario_d"
-    when 1206.7 > dd_value && dd_value >= 507.2 && end_date <= july_15 then "senario_e"
-    when dd_value >= 1206.7 && end_date <= july_15 then "senario_f"
-    when end_date > july_15 then "senario_g"
+    when dd_value < 128.3 && end_date <= july_15 then return "scenario_a"
+    when 215.8 > dd_value && dd_value >= 128.3 && end_date <= july_15 then "scenario_b"
+    when 354.2 > dd_value && dd_value >= 215.8 && end_date <= july_15 then "scenario_c"
+    when 507.2 > dd_value && dd_value >= 354.2 && end_date <= july_15 then "scenario_d"
+    when 1206.7 > dd_value && dd_value >= 507.2 && end_date <= july_15 then "scenario_e"
+    when dd_value >= 1206.7 && end_date <= july_15 then "scenario_f"
+    when end_date > july_15 then "scenario_g"
     end
   end
 
