d961e3c115c47f7878eefcee2ad8cd915a954f34
bbryant7 <bailey.bryant10@gmail.com>
2020-11-11 10:37:26 -0600

    add mysql gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,6 @@ set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 
-# SSHKit.config.command_map[:bundle] = "/home/deploy/soils_ag_wx/shared/bundle/ruby/2.6.0"
-# set :linked_dirs, fetch(:linked_dirs, []) << '/home/deploy/soils_ag_wx/shared/bundle/ruby/2.6.0'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
