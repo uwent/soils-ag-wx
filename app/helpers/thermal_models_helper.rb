b204e16786c43ec19a889da725b441a417fc7040
Ben Bradford <bradfordium@gmail.com>
2021-05-06 13:26:16 -0500

    dd display tweaks

--- app/helpers/thermal_models_helper.rb
+++ app/helpers/thermal_models_helper.rb
@@ -1,19 +1,8 @@
 module ThermalModelsHelper
-  def ddgif_image_link(image_file,alt="Degree-Day Map")
-    url =<<-END
-    <a href="http://agwx.soils.wisc.edu/soils-agwx-assets/uwex_agwx/images/ddgifs/#{image_file}">
-      <IMG SRC="http://agwx.soils.wisc.edu/soils-agwx-assets/uwex_agwx/images/ddgifs/#{image_file}" ALT="#{alt}" height="400" width="480"></a>
-      END
-    url.html_safe
-  end
 
-  def ddgif_url(link_text,image_file)
-    link_to link_text, "http://agwx.soils.wisc.edu/soils-agwx-assets/uwex_agwx/images/ddgifs/#{image_file}"
-  end
-
+  # oak wilt scenarios
   def define_scenario(dd_value, end_date)
     july_15 = (end_date.year.to_s + "-07-15").to_date
-
     return "scenario_g" if end_date > july_15 # after jul 15
     return "scenario_a" if dd_value < 231 # before flight
     return "scenario_b" if dd_value < 368 # 5-25% flight
@@ -23,4 +12,5 @@ module ThermalModelsHelper
     return "scenario_f" if dd_value >= 2172 # > 95% flight
     return "scenario_a"
   end
+
 end
