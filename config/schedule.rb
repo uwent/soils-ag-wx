89d51ddd78d48caa8a52492f69abc5537270ed02
Ben Bradford <bradfordium@gmail.com>
2021-05-20 09:36:43 -0500

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,10 +21,10 @@
 
 
 set :output, "/tmp/mailer_whenever.log"
-set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
+set :env_path, '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
 job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
 
-every :day, at: '7:00am' do
+every :day, at: '7:30am' do
   runner "Subscriber.send_daily_mail"
 end
 
