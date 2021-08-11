63b4e7e029c955c9f1f1b8f7717a125114ea7b48
Ben Bradford <bradfordium@gmail.com>
2021-08-11 17:15:52 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ set :deploy_to, "/home/deploy/soils_ag_wx"
 # set :log_level, :debug
 # set :pty, true
 
-set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 
