e501f435aa06b0e3194bceeed600573d368e54a4
Ben Bradford <bradfordium@gmail.com>
2022-04-06 19:14:57 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set the default host and port to be the same as Action Mailer.
+SoilsAgWx::Application.default_url_options = SoilsAgWx::Application.config.action_mailer.default_url_options
