534912efec48c1bd77710d72573581344909c140
Ben Bradford <bradfordium@gmail.com>
2022-04-14 18:52:08 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Fill the WiMnDAveTAirs grid with 2002 data (clone of import_grid.rb)
-min_path = File.join(File.dirname(__FILE__), "WIMNTMin2002")
-WiMnDAveTAir.import_grid(min_path, 2002)
+# min_path = File.join(File.dirname(__FILE__), "WIMNTMin2002")
+# WiMnDAveTAir.import_grid(min_path, 2002)
 
 # Product.create!(
 #   name: "Cranberry Consumptive Water Use",
