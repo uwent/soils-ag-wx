4cf950ea3f5d1d1a140d5ad16699e48ffea438d8
Ben Bradford <bradfordium@gmail.com>
2022-12-08 15:59:57 -0600

    move date checking script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,33 @@ function copy(id) {
 if (window.history.replaceState) {
   window.history.replaceState(null, null, window.location.href);
 }
+
+function parseDate(el) {
+  let year = el.querySelector("[id$='date_1i']").value
+  let month = el.querySelector("[id$='date_2i']").value
+  let day = el.querySelector("[id$='date_3i']").value
+  let date = new Date(`${month}-${day}-${year}`)
+  return (date.getDate() == day) ? date : false
+}
+
+function validateDates(el) {
+  let startDate = el.querySelector("#start-date")
+  let endDate = el.querySelector("#end-date")
+  let startDateValue = parseDate(startDate)
+  let endDateValue = parseDate(endDate)
+  let errors = ""
+  errors += startDateValue ? "" : "Invalid start date. "
+  errors += endDateValue ? "" : "Invalid end date. "
+
+  if (startDateValue && endDateValue) {
+    errors += (startDateValue < endDateValue) ? "" : "Start date must be before end date. "
+  }
+
+  if (errors.length > 0) {
+    el.querySelector("#date-warning").innerHTML = errors
+    el.querySelector("#submit").disabled = true
+  } else {
+    el.querySelector("#date-warning").innerHTML = ""
+    el.querySelector("#submit").disabled = false
+  }
+}
