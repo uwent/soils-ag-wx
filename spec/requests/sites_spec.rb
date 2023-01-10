7aa5fbf1f138909d70c2116066657d042af699a6
Ben Bradford <bradfordium@gmail.com>
2023-01-09 20:02:00 -0600

    Update sites_spec.rb

--- spec/requests/sites_spec.rb
+++ spec/requests/sites_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Sites", type: :request do
     it "gets a site by params" do
       get url_for(controller: :sites, action: :show, lat: 45.0, long: -89.5)
       expect(response).to have_http_status(:success)
-      expect(response.body).to include("45.0, -89.5")
+      expect(response.body).to include("45.0&deg;N, -89.5&deg;W".html_safe)
     end
   end
 end
