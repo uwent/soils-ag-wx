4cf41af360c0e10c9acc2526f0013e6d49bf84c6
Ben Bradford <bradfordium@gmail.com>
2023-10-11 18:28:39 -0500

    bumps rails to v7.1 and update binstubs

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module SoilsAgWx
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,6 +23,6 @@ module SoilsAgWx
 
     config.time_zone = "Central Time (US & Canada)"
 
-    config.autoload_paths << Rails.root.join("app/models/subscriptions")
+    config.eager_load_paths << Rails.root.join("app/models/subscriptions")
   end
 end
