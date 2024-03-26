365c1de29b49fc134a9e9b1208553f987ee99f4a
Ben Bradford <bbradford@wisc.edu>
2024-03-26 14:10:33 -0500

    fix logger

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,10 +1,11 @@
-# Just use the production settings
+# Load production defaults
 require File.expand_path("../production.rb", __FILE__)
 
-# Overwrite production defaults
+# Staging settings
 Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: ENV["AG_WEATHER_HOST"] || "dev.agweather.cals.wisc.edu",
     protocol: "https"
   }
+  config.log_level = :debug
 end
